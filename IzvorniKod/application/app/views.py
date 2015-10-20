0c972be9eafe2eaa10bbc04e9458225d2d8b2145
Zvonimir <zjurelinac@gmail.com>
2015-10-20 11:53:43 +0200

    Storing changes before pull

---
+++ IzvorniKod/application/app/views.py
@@ -0,0 +1,22 @@
+# from flask import
+from app import app, db
+
+@app.route( '/' )
+def index():
+    return 'Hello'
+
+# Login process
+
+# register process
+
+# register process 2 - passgen
+
+# tracks
+
+# users
+
+# wishlist
+
+# tracklist
+
+# scheduling
