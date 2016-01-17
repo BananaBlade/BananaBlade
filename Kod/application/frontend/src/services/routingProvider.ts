5cf330fa3a1be9978fff2cf1ada4d9160dc8fd91
Zvonimir Jurelinac <zjurelinac@gmail.com>
2016-01-17 22:29:06 +0100

    DB Fix

--- Kod/application/frontend/src/services/routingProvider.ts
+++ Kod/application/frontend/src/services/routingProvider.ts
@@ -53,7 +53,7 @@ class NavGroup {
 
 export class NavigationProvider {
     static navigationArray: NavGroup[] = [
-        new NavGroup( 'Pregled mogućnosti', [], 2+4+8+16, true, new MyAsyncComponent( '', 'Overview', 'overview', './settings' ) ),
+        new NavGroup( 'Pregled mogućnosti', [], 2+4+8+16, true, new MyAsyncComponent( '', 'Overview', 'overview', './' ) ),
         new NavGroup( 'Vlasničke mogućnosti', [
             new MyAsyncComponent( 'Administratori', 'ManageAdmins', 'manageAdmins', './admins' ),
             new MyAsyncComponent( 'Podaci o postaji', 'ManageRadiostation', 'manageRadiostation', './station' )
