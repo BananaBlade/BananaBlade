1da03dfd80a711df3f4909d60925b4a7357544f0
Dominik Ivošević <dominik.dito@gmail.com>
2016-01-17 04:26:03 +0100

    Async routing!!!!

--- Kod/application/frontend/src/components/headerBar/headerBar.ts
+++ Kod/application/frontend/src/components/headerBar/headerBar.ts
@@ -1,12 +1,9 @@
 import { Component, Input } from 'angular2/core';
-import {
-    FORM_DIRECTIVES, COMMON_DIRECTIVES,
-    FormBuilder, ControlGroup, Validators, Control
-} from 'angular2/common';
+import { FORM_DIRECTIVES, COMMON_DIRECTIVES, FormBuilder, ControlGroup, Validators, Control } from 'angular2/common';
 import { Http } from 'angular2/http';
 import 'rxjs/Rx';
 
-import { Form } from '../../services/utilities';
+import { Form, urlEncode } from '../../services/utilities';
 
 @Component({
     selector: 'header-bar',
@@ -18,10 +15,21 @@ export class HeaderBar {
     @Input() isFixed : boolean;
     @Input() isLoggedIn : boolean;
     loginForm: Form;
+    http: Http;
 
     constructor(fb: FormBuilder, http: Http) {
+        this.http = http;
+
         let loginEntities = ['email', 'password'];
         this.loginForm = new Form(fb, http, loginEntities, '/user/auth/login');
         console.log( this.isLoggedIn );
+    }
+
+    onSubmit(value: String): void {
+        console.log(value);
+        this.http.post('/user/auth/login', urlEncode(value)).map((res) => res.json()).subscribe((res) => {
+            console.log(res);
+            document.location.reload(true);
+        }, (err) => console.log(err));
     }
 }
